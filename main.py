a28c5345493b864244e35e0200d62be7c351164b
meiraa :) <98461224+mangaliits@users.noreply.github.com>
2022-02-02 15:34:15 +0000

    izpildīju 3.uzd

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+'''#2.uzd
 import mod
 n= []
 n.append(int(input("ievadi 1. skaitli: ")))
@@ -6,3 +7,24 @@ x=int(input("kuru virknes n elementu vēlies noskaidrot?: "))
 
 rez= mod.elem(n,x)
 print(f"virknes {x}. elements ir:", n[x-1])
+
+print(" ")'''
+
+
+#3.uzd
+skaitlis= str(input("ievadi skaitli: "))
+summ=int(skaitlis)
+count=0
+
+while(summ>0):
+    count=count+1
+    summ=summ//10
+if (count>100):
+  print("parak liels skaitlis:",count,"didžiti. ievadi skaitli zem 100 didžitiem" )
+
+s=0
+for i in range(len(skaitlis)):
+  a=(skaitlis[i:])
+  print(a)
+
+#viss strādā, vienīgi neizdodas uzrakstīt koda daļu, kur šos skaitļus saskaita. Nesaprotu, kuru jāizmanto, int vai str.
